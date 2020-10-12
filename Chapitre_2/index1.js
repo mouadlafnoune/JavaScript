cf254c9156bdb4c0e01a7b3dae8e6df15049cf5d
mouadlafnoune <mouad.lafnoune1@gmail.com>
2020-10-12 17:15:22 +0100

    chapitre object

---
+++ Chapitre_2/index1.js
@@ -0,0 +1,27 @@
+// Object
+// let user = {
+//     firstName: "Mouad",
+//     lastName: "Lafnoune",
+//     age: 22,
+//     email: "mouad@gmail.com",
+//     active: true,
+//     courses: [
+//         {title: "Angular", price: 45},
+//         {title: "ReactJS", price: 35},
+//         {title: "VueJS", price: 40},
+//     ],
+//     login: () => {
+//         console.log('log with this user.')
+//     },
+//     showCourses: function()  {
+//         this.courses.forEach(course => console.log(course.title, course.price));
+//     }
+// }
+// console.log(user.showCourses());
+// console.log(user.login());
+
+// user.email = "laf@gmail.com";
+// console.log(user);
+// console.log(user['firstName']);
+// user['age'] = 23;
+// console.log(user)
