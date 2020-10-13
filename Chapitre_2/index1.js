97defc19b1ac8d87856a86063983b2f181dd26bf
mouadlafnoune <mouad.lafnoune1@gmail.com>
2020-10-14 00:44:51 +0100

    chapitre DOM

--- Chapitre_2/index1.js
+++ Chapitre_2/index1.js
@@ -25,3 +25,34 @@
 // console.log(user['firstName']);
 // user['age'] = 23;
 // console.log(user)
+
+// const header = document.querySelector('h1');
+// const paras = document.querySelectorAll('div.myContent > h2.title');
+// paras.forEach(para => console.log(para))
+
+/************ change VALUE */
+
+// const header = document.querySelector('#title');
+// console.log(header.innerText += " JavaScript")
+
+// const headers = document.querySelectorAll('h2');
+// headers.forEach((header,index) => {
+//     header.innerText += ' .'
+// });
+
+// const division = document.querySelector('.myContent')
+// console.log(division.innerHTML)
+// division.innerHTML += "<a href='https://brightcoding.teachable.com'>Bright Coding</a>"
+
+
+/*** change attr */
+
+const anchor = document.querySelector('a');
+console.log(anchor.getAttribute('href'))
+anchor.setAttribute('href', 'https://facebook.com')
+anchor.innerText = 'facebook'
+
+
+const para = document.querySelector('p');
+para.setAttribute('class', 'error')
+para.setAttribute('style','color: red')
